a1686f1ab59df4fa7d005834b35d02883125c195
m1mik1 <156483345+m1mik1@users.noreply.github.com>
2024-09-18 20:04:30 +0300

    Update HollowTriangle.java

--- HollowTriangle.java
+++ HollowTriangle.java
@@ -1,28 +1,26 @@
-package dev.stillsucking.PrintingPattern;
+package dev.stillsucking.printing.patterns;
 
 import java.util.Scanner;
 
 public class HollowTriangle {
-  Scanner scanner = new Scanner(System.in);
+      public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a height: ");
         int h = scanner.nextInt();
         for (int j = 0; j < h; j++) {
             System.out.print("*");
-           // System.out.print(" ");
-            for (int i = 0; i <=j; i++) {
-                if (i==j && i!=0 ) {
-
+            for (int i = 0; i <= j; i++) {
+                if (i == j && i != 0) {
                     System.out.print("*");
-                }
-                else if (j==h-1) {
+                } else if (j == h - 1) {
                     System.out.print("*");
-                }
-             else if(i!=j ) {
+                } else if (i != j) {
                     System.out.print(" ");
                 }
             }
-            System.out.println(" ");
-        }
+        System.out.println(" ");
+    }
+  }
 }
 
-}
+
