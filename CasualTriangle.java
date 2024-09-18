1d590d6a6dbbe4f5af26419ab2c60e10e27e0bdd
m1mik1 <156483345+m1mik1@users.noreply.github.com>
2024-09-18 20:06:08 +0300

    Update CasualTriangle.java

--- CasualTriangle.java
+++ CasualTriangle.java
@@ -1,20 +1,17 @@
-package dev.stillsucking.PrintingPattern;
+package dev.stillsucking.printing.patterns;
 
 import java.util.Scanner;
 
 public class CasualTriangle {
     public static void main(String[] args) {
-  Scanner scanner = new Scanner(System.in);
-   System.out.println("Enter a number: ");
-    int h = scanner.nextInt();
-        for (int k = 0; k < h; k++) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a height: ");
+        int d = scanner.nextInt();
+        for (int k = 0; k <= d; k++) {
             for (int i = 0; i < k; i++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
-
-
     }
 }
